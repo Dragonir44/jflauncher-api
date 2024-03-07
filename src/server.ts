1a372982e43e36c48f799261fef03c3396189c27
Dragonir44 <dragonir44@gmail.com>
2024-03-07 09:51:54 +0100

    Ajouter la lecture du jeton à partir d'un fichier

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import dotenv from 'dotenv';
 import app from './app/index';
+import fs from 'fs';
 
 // load environment variables
 dotenv.config()
+
+if (fs.existsSync(<string>process.env.TOKEN_FILE)) {
+  process.env.TOKEN = fs.readFileSync(<string>process.env.TOKEN_FILE, 'utf8').trim()
+  console.log('Token loaded from file')
+}
+
+console.log(process.env.TOKEN)
 
 const PORT = process.env.PORT || 5000;
 
