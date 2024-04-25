9158295c6ce77d5873bd37e8925299ae817269ea
fthaumur <flavien.thaumur@nextdecision.fr>
2024-04-25 10:44:17 +0200

    Mise à jour du fichier routes/news.router.ts pour ajouter les routes GET, POST, DELETE et PUT pour les actualités. Ajout du schéma de l'objet News dans le fichier schema/news.shema.ts.

--- src/app/routes/news.router.ts
+++ src/app/routes/news.router.ts
@@ -1,7 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import fs from "fs";
-import path from "path";
 
 import {
     getNews,
@@ -15,6 +13,73 @@ dotenv.config();
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * /news:
+ *  parameters:
+ *   - in: header
+ *     name: token
+ *     required: true
+ *     schema:
+ *      type: uuid
+ *     description: Token to access the API
+ *  get:
+ *   tags:
+ *    - News
+ *   summary: Get all news
+ *   responses:
+ *    200:
+ *     description: A list of news
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: array
+ *        items:
+ *         $ref: '#/components/schemas/getNewsResponse'
+ *    401:
+ *     description: Unauthorized
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        properties:
+ *         message:
+ *          type: string
+ *          example: Unauthorized
+ *    404:
+ *     description: Not found
+ *      
+ *  post:
+ *   tags:
+ *    - News
+ *   summary: Add a news
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/components/schemas/News'
+ *   responses:
+ *    200:
+ *     description: The news has been added
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/News'
+ *    401:
+ *     description: Unauthorized
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        properties:
+ *         message:
+ *          type: string
+ *          example: Unauthorized
+ *    404:
+ *     description: Not found
+ */
+
 router.route("/")
     .get((req, res) => {
         if (req.headers['token'] == process.env.TOKEN) {
@@ -27,6 +92,112 @@ router.route("/")
     .post((req, res) => {
         res.json(addNews(req.body));
     });
+
+/**
+ * @openapi
+ * /news/{id}:
+ *  parameters:
+ *   - in: header
+ *     name: token
+ *     required: true
+ *     schema:
+ *      type: uuid
+ *      description: Token to access the API
+ *   - in: path
+ *     name: id
+ *     required: true
+ *     schema:
+ *      type: integer
+ *      description: News ID
+ *      example: 0
+ *      minimum: 0
+ *  get:
+ *   tags:
+ *    - News
+ *   summary: Get a news by ID
+ *   responses:
+ *    200:
+ *     description: A news
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/News'
+ *    401:
+ *     description: Unauthorized
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        properties:
+ *         message:
+ *          type: string
+ *          example: Unauthorized
+ *    404:
+ *     description: Not found
+ * 
+ *  delete:
+ *   tags:
+ *    - News
+ *   summary: Delete a news by ID
+ *   responses:
+ *    204:
+ *     description: The news has been deleted
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        properties:
+ *         message:
+ *          type: string
+ *          example: News deleted
+ *     401:
+ *      description: Unauthorized
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: object
+ *         properties:
+ *          message:
+ *           type: string
+ *           example: Unauthorized
+ *     404:
+ *      description: Not found
+ * 
+ *  put:
+ *   tags:
+ *    - News
+ *   summary: Update a news by ID
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/components/schemas/News'
+ *   responses:
+ *    204:
+ *     description: The news has been updated
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        properties:
+ *         message:
+ *          type: string
+ *          example: News updated
+ *    401:
+ *     description: Unauthorized
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        properties:
+ *         message:
+ *          type: string
+ *          example: Unauthorized
+ *    404:
+ *     description: Not found
+ * 
+ */
 
 router.route("/:id")
     .get((req, res) => {
---
+++ src/app/schema/news.shema.ts
@@ -0,0 +1,33 @@
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *   News:
+ *    type: object
+ *    properties:
+ *     lang:
+ *       type: object
+ *       properties:
+ *        fr:
+ *         type: object
+ *         properties:
+ *          title:
+ *           type: string
+ *           example: "Titre"
+ *          content:
+ *           type: string
+ *           example: "Contenu"
+ *        en:
+ *         type: object
+ *         properties:
+ *          title:
+ *           type: string
+ *           example: "Title"
+ *          content:
+ *           type: string
+ *           example: "Content"
+ *     date:
+ *      type: string
+ *      example: "2021-01-01"
+ * 
+ */
