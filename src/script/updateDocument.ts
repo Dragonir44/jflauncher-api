bd2d80ee4e87ef490c460daaaabd3f8945ad4e9d
fthaumur <flavien.thaumur@nextdecision.fr>
2024-06-04 14:19:58 +0200

    chore: Rename versions property to use lowercase "version"

--- src/script/updateDocument.ts
+++ src/script/updateDocument.ts
@@ -2,26 +2,6 @@ import {collections} from '../app/services/db.service';
 
 const updateDocument = async () => {
     try {
-        // await collections.channel?.bulkWrite([
-        //     {
-        //         updateMany: {
-        //             filter: {},
-        //             update: [
-        //                 {
-        //                     $rename: {
-        //                         "versions.$[element].Version": "versions.$[element].version",
-        //                         // "versions.$[element].changelog.fr": "versions.$[element].Changelog.Fr",
-        //                         // "versions.$[element].changelog.en": "versions.$[element].Changelog.En",
-        //                         // "versions.$[element].path": "versions.$[element].Path",
-        //                         // "versions.$[element].forgeVersion": "versions.$[element].ForgeVersion"
-        //                     }
-        //                 }
-        //             ],
-        //             arrayFilters: [{ "element.version": { $exists: true } }]
-        //         }
-        //     }
-        // ])
-
         await collections.channel?.updateMany({}, { 
             $rename: { 
                 "versions.$[element].Changelog.Fr": "versions.$[element].Changelog.fr" 
